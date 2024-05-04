46b13d018eb0ac212b70da9de27a2fd8f5105fa9
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-05-05 00:19:40 +0200

    handle CancelHoldCommand

---
+++ Library.Modules.Reservation.Application/Patrons/CancelHoldCommand.cs
@@ -0,0 +1,17 @@
+﻿using Library.Modules.Reservation.Application.Contracts;
+
+namespace Library.Modules.Reservation.Application.Patrons;
+
+public class CancelHoldCommand : CommandBase
+{
+    public Guid BookId { get; private set; }
+    public Guid LibraryBranchId { get; private set; }
+    public Guid PatronId { get; private set; }
+
+    private CancelHoldCommand(Guid bookId, Guid libraryBranchId, Guid patronId)
+    {
+        BookId = bookId;
+        LibraryBranchId = libraryBranchId;
+        PatronId = patronId;
+    }
+}
---
+++ Library.Modules.Reservation.Application/Patrons/CancelHoldCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Library.Modules.Reservation.Application.Contracts;
+using Library.Modules.Reservation.Domain.Books;
+using Library.Modules.Reservation.Domain.Holds;
+using Library.Modules.Reservation.Domain.Patrons;
+
+namespace Library.Modules.Reservation.Application.Patrons;
+
+public class CancelHoldCommandHandler(IPatronRepository patronRepository, IHoldRepository holdRepository)
+    : ICommandHandler<CancelHoldCommand>
+{
+    public async Task Handle(CancelHoldCommand command, CancellationToken cancellationToken)
+    {
+        var patron = await patronRepository.GetByIdAsync(new PatronId(command.PatronId));
+        var activeHolds = await holdRepository.GetActiveHoldsByPatronIdAsync(new PatronId(command.PatronId));
+        
+        var bookOnHold = BookOnHold.Create(new BookId(command.BookId), new LibraryBranchId(command.LibraryBranchId),
+            new PatronId(command.PatronId));
+        
+        patron.CancelHold(bookOnHold, activeHolds);
+    }
+}
