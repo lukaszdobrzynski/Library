85beffaa8602e60a3fb8aabdbbadad36433da7ca
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-05-05 18:26:50 +0200

    add IReservationModule contract

---
+++ Library.Modules.Reservation.Application/Contracts/IReservationModule.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Modules.Reservation.Application.Contracts;
+
+public interface IReservationModule
+{
+    Task ExecuteCommandAsync(ICommand command);
+
+    Task<TResult> ExecuteCommandAsync<TResult>(ICommand<TResult> command);
+
+    Task<TResult> ExecuteQueryAsync<TResult>(IQuery<TResult> query);
+}
