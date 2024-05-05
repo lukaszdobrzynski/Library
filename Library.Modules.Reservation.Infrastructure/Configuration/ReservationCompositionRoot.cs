c649609445eaea5d72d1ccfb38b6269c6263caac
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-05-05 19:23:57 +0200

    add ReservationCompositionRoot

---
+++ Library.Modules.Reservation.Infrastructure/Configuration/ReservationCompositionRoot.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace Library.Modules.Reservation.Infrastructure.Configuration;
+
+internal static class ReservationCompositionRoot
+{
+    private static IContainer _container;
+
+    public static void SetContainer(IContainer container)
+    {
+        _container = container;
+    }
+
+    public static ILifetimeScope BeginLifetimeScope()
+    {
+        return _container.BeginLifetimeScope();
+    }
+}
