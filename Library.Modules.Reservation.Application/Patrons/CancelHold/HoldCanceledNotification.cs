94280e83baaf05842c0376a783939a963c5678ae
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-06-02 12:59:02 +0200

    rename domain event notification related to hold cancelling

--- Library.Modules.Reservation.Application/Patrons/CancelHold/HoldCanceledNotification.cs
+++ Library.Modules.Reservation.Application/Patrons/CancelHold/HoldCanceledNotification.cs
@@ -3,12 +3,12 @@ using Library.Modules.Reservation.Domain.Patrons.Events;
 
 namespace Library.Modules.Reservation.Application.Patrons.CancelHold;
 
-public class BookHoldCanceledByPatronNotification : IDomainEventNotification<HoldCanceledDomainEvent>
+public class HoldCanceledNotification : IDomainEventNotification<HoldCanceledDomainEvent>
 {
     public Guid Id { get; set; }
     public HoldCanceledDomainEvent DomainEvent { get; }
 
-    public BookHoldCanceledByPatronNotification(HoldCanceledDomainEvent domainEvent, Guid id)
+    public HoldCanceledNotification(HoldCanceledDomainEvent domainEvent, Guid id)
     {
         DomainEvent = domainEvent;
         Id = id;
