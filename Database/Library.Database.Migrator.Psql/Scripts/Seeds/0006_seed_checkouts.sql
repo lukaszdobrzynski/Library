412a82736604339a27c8c98c15d80c598e20e246
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-08-17 20:38:43 +0200

    adjust books test data

--- Database/Library.Database.Migrator.Psql/Scripts/Seeds/0006_seed_checkouts.sql
+++
@@ -1,3 +0,0 @@
-﻿INSERT INTO reservations.checkouts (id, patron_id, book_id, library_branch_id, due_date, version_id) VALUES
-('9b030b5c-2e38-4b80-845d-47a0c91d776d', 'f0e4109e-4975-439a-aa11-d369f8d6a9ec', '01ff5291-1051-4e5d-99ad-1a97874cc8cd', 'ed4a1e1e-0c2b-40ef-8b34-567f2b6ef1e0', '2024-05-10', 1),
-('7b77c706-1e4b-4c1e-9255-1d13d3b7f657', 'b4be6646-16a7-42fc-897a-0b65a931f85f', '529206ef-0d02-420a-aa6b-005e3c054867', 'ed4a1e1e-0c2b-40ef-8b34-567f2b6ef1e0', '2024-05-13', 1);
