a1dff346a17317958a65f4229acc98e6229cd5b2
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-08-20 18:44:20 +0200

    remove unused commands and command handlers

--- Library.Modules.Reservation.Application/Holds/LoanHold/ApplyLoanHoldDecisionCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using Library.Modules.Reservation.Application.Contracts;
-
-namespace Library.Modules.Reservation.Application.Holds.LoanHold;
-
-public class ApplyLoanHoldDecisionCommand(Guid holdId) : CommandBase
-{
-    public Guid HoldId { get; private set; } = holdId;
-}
