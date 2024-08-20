f1b2b75303883e6bcf19270e1454465735dc0acd
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-08-20 17:35:56 +0200

    resolve scope inside DomainEventToDomainEventNotificationResolver

--- Library.Modules.Reservation.Infrastructure/Events/DomainEventToDomainEventNotificationResolver.cs
+++ Library.Modules.Reservation.Infrastructure/Events/DomainEventToDomainEventNotificationResolver.cs
@@ -2,23 +2,18 @@
 using Autofac.Core;
 using Library.BuildingBlocks.Domain;
 using Library.Modules.Reservation.Application.Contracts;
+using Library.Modules.Reservation.Infrastructure.Configuration;
 
 namespace Library.Modules.Reservation.Infrastructure.Events;
 
 public class DomainEventToDomainEventNotificationResolver : IDomainEventToDomainEventNotificationResolver
 {
-    private readonly ILifetimeScope _scope;
-    
-    public DomainEventToDomainEventNotificationResolver(ILifetimeScope scope)
-    {
-        _scope = scope;
-    }
-
     public IDomainEventNotification<IDomainEvent>? ResolveOptional(IDomainEvent domainEvent)
     {
+        var scope = ReservationCompositionRoot.BeginLifetimeScope();
         var domainEventNotificationType = typeof(IDomainEventNotification<>);
         var domainEventNotificationGenericType = domainEventNotificationType.MakeGenericType(domainEvent.GetType());
-        var domainNotification = _scope.ResolveOptional(domainEventNotificationGenericType, new List<Parameter>
+        var domainNotification = scope.ResolveOptional(domainEventNotificationGenericType, new List<Parameter>
         {
             new NamedParameter("domainEvent", domainEvent),
             new NamedParameter("id", domainEvent.Id)
