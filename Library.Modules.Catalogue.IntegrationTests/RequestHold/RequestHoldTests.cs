19850b8d0b43bbeb9412261631304ca2836c457c
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-08-25 22:29:22 +0200

    test RequestHoldCommandHandler

--- Library.Modules.Catalogue.IntegrationTests/RequestHold/RequestHoldTests.cs
+++ Library.Modules.Catalogue.IntegrationTests/RequestHold/RequestHoldTests.cs
@@ -1,22 +1,87 @@
 ﻿using System.Threading.Tasks;
+using Library.Modules.Catalogue.Application.PlaceBookOnHold;
+using Library.Modules.Catalogue.Infrastructure.Configuration;
+using Library.Modules.Catalogue.Models;
 using NUnit.Framework;
 
 namespace Library.Modules.Catalogue.IntegrationTests.RequestHold;
 
+[TestFixture]
 public class RequestHoldTests : TestBase
 {
     [Test]
-    public async Task Succeeds()
+    public async Task Succeeds_WhenBookIsAvailable()
     {
-        using (var session = OpenAsyncSession())
-        {
-            await session.StoreAsync(new TestData { Name = "Test Data 1"});
-            await session.SaveChangesAsync();
-        }
-    }
-}
+        var book = new Book { Id = SomeBookId.ToString(), Status = BookStatus.Available };
 
-public class TestData
-{
-    public string Name { get; set; }
+        await StoreAsync(book);
+
+        var command = RequestHoldCommand.CreateSubmitted(
+            SomeBookId,
+            SomeLibraryBranchId,
+            SomePatronId,
+            HoldTillWeekFromNow,
+            SomeExternalHoldRequestId);
+
+        await Act(command);
+
+        var notification = await GetSingleOutboxMessage<BookHoldGrantedNotification>();
+        Assert.That(notification.BookId, Is.EqualTo(SomeBookId));
+
+        var bookOnHold = await LoadAsync<Book>(book.Id);
+        Assert.That(bookOnHold.Status, Is.EqualTo(BookStatus.OnHold));
+    }
+
+    [Test]
+    public async Task Succeeds_WhenBookIsOnHold()
+    {
+        var book = new Book { Id = SomeBookId.ToString(), Status = BookStatus.OnHold };
+
+        await StoreAsync(book);
+
+        var command = RequestHoldCommand.CreateSubmitted(
+            SomeBookId,
+            SomeLibraryBranchId,
+            SomePatronId,
+            HoldTillWeekFromNow,
+            SomeExternalHoldRequestId);
+
+        await Act(command);
+
+        var notification = await GetSingleOutboxMessage<BookHoldRejectedNotification>();
+        Assert.That(notification.BookId, Is.EqualTo(SomeBookId));
+
+        var bookOnHold = await LoadAsync<Book>(book.Id);
+        Assert.That(bookOnHold.Status, Is.EqualTo(BookStatus.OnHold));
+    }
+    
+    [Test]
+    public async Task Succeeds_WhenBookIsCheckedOut()
+    {
+        var book = new Book { Id = SomeBookId.ToString(), Status = BookStatus.CheckedOut };
+
+        await StoreAsync(book);
+
+        var command = RequestHoldCommand.CreateSubmitted(
+            SomeBookId,
+            SomeLibraryBranchId,
+            SomePatronId,
+            HoldTillWeekFromNow,
+            SomeExternalHoldRequestId);
+
+        await Act(command);
+
+        var notification = await GetSingleOutboxMessage<BookHoldRejectedNotification>();
+        Assert.That(notification.BookId, Is.EqualTo(SomeBookId));
+
+        var bookOnHold = await LoadAsync<Book>(book.Id);
+        Assert.That(bookOnHold.Status, Is.EqualTo(BookStatus.CheckedOut));
+        WaitForUserToContinueTheTest();
+    }
+    
+    private Task Act(RequestHoldCommand command)
+    {
+        var commandHandler = new RequestHoldCommandHandler(DocumentStoreHolder, new DomainNotificationsRegistry());
+        return commandHandler.Handle(command);
+    }
 }
--- Library.Modules.Catalogue.IntegrationTests/TestBase.cs
+++ Library.Modules.Catalogue.IntegrationTests/TestBase.cs
@@ -1,11 +1,25 @@
-﻿using Library.BuildingBlocks.IntegrationTests;
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Library.BuildingBlocks.IntegrationTests;
+using Library.Modules.Catalogue.Application.Contracts;
+using Library.Modules.Catalogue.Infrastructure;
 using Library.Modules.Catalogue.Infrastructure.Configuration;
+using Library.Modules.Catalogue.Models;
+using Newtonsoft.Json;
 using NUnit.Framework;
+using Raven.Client.Documents;
 
 namespace Library.Modules.Catalogue.IntegrationTests;
 
 public class TestBase : RavenTestBase
 {
+    protected Guid SomeBookId => Guid.Parse("ae0236d1-badc-4cb1-a5e4-e4137f2b1942");
+    protected Guid SomePatronId => Guid.Parse("60f21521-529f-4fae-8420-add4d067bf20");
+    protected Guid SomeLibraryBranchId => Guid.Parse("a6b22b9c-52e1-438d-982d-668bcdb0821e");
+    protected Guid SomeExternalHoldRequestId => Guid.Parse("8b2674a5-0bf6-4609-b539-0f415b1b6b9b");
+    protected DateTime HoldTillWeekFromNow => DateTime.UtcNow.AddDays(7);
+    
     [SetUp]
     protected override void BeforeEachTest()
     {
@@ -15,5 +29,70 @@ public class TestBase : RavenTestBase
             new MockExecutionContextAccessor(),
             new MockLogger(),
             new MockEventBus());
+    }
+
+    protected CatalogueModule CatalogueModule => new();
+
+    protected async Task StoreAsync<T>(T entity)
+    {
+        using (var session = OpenAsyncSession())
+        {
+            await session.StoreAsync(entity);
+            await session.SaveChangesAsync();
+        }
+    }
+
+    protected async Task<T> LoadSingleAsync<T>()
+    {
+        using (var session = OpenAsyncSession())
+        {
+             var document = await session.Query<T>().SingleAsync();
+             return document;
+        }
+    }
+
+    protected async Task<T> LoadAsync<T>(string id)
+    {
+        using (var session = OpenAsyncSession())
+        {
+            var document = await session.LoadAsync<T>(id);
+            Assert.That(document, Is.Not.Null);
+            return document;
+        }
+    }
+    
+    protected async Task AssertExistsInDb(string id)
+    {
+        using (var session = OpenAsyncSession())
+        {
+            var exists = await session.Advanced.ExistsAsync(id);
+            Assert.That(exists, Is.True);
+        }
+    }
+
+    protected async Task AssertDoesNotExistInDb(string id)
+    {
+        using (var session = OpenAsyncSession())
+        {
+            var exists = await session.Advanced.ExistsAsync(id);
+            Assert.That(exists, Is.False);
+        }
+    }
+    
+    protected async Task AssertSingleDocumentInDb<T>()
+    {
+        using (var session = OpenAsyncSession())
+        {
+            var documents = await session.Query<T>().ToListAsync();
+            Assert.That(documents, Has.Exactly(1).Items);
+        }
+    }
+    
+    protected async Task<T> GetSingleOutboxMessage<T>()
+        where T : class, IDomainNotification
+    {
+        var message = await LoadSingleAsync<OutboxMessage>();
+        var type = Assembly.GetAssembly(typeof(T)).GetType(typeof(T).FullName);
+        return JsonConvert.DeserializeObject(message.Data, type) as T;
     }
 }
