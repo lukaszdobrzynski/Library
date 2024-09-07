8dcd8958533e3c268720c37a4f6f58c77b8dbe06
Lukasz Dobrzynski <lukasz@ravendb.net>
2024-09-07 11:39:37 +0200

    add catalogue module arch tests

---
+++ Library.Modules.Catalogue.ArchTests/LayerTests.cs
@@ -0,0 +1,29 @@
+﻿using NetArchTest.Rules;
+using NUnit.Framework;
+
+namespace Library.Modules.Catalogue.ArchTests;
+
+public class LayerTests : TestBase
+{
+    [Test]
+    public void ApplicationLayer_DoesNotDependOn_InfrastructureLayer()
+    {
+        var result = Types.InAssembly(ApplicationLayer)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureLayer.FullName)
+            .GetResult();
+        
+        AssertEmptyArchTestResult(result);
+    }
+
+    [Test]
+    public void InfrastructureLayer_DependsOn_ApplicationLayer()
+    {
+        var result = Types.InAssembly(InfrastructureLayer)
+            .Should()
+            .HaveDependencyOn(ApplicationLayer.FullName)
+            .GetResult();
+        
+        AssertNotEmptyArchTestResult(result);
+    }
+}
---
+++ Library.Modules.Catalogue.ArchTests/TestBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Library.Modules.Catalogue.Application.Contracts;
+using Library.Modules.Catalogue.Infrastructure;
+using NetArchTest.Rules;
+using NUnit.Framework;
+
+namespace Library.Modules.Catalogue.ArchTests;
+
+public abstract class TestBase
+{
+    protected readonly Assembly ApplicationLayer = typeof(ICatalogueModule).Assembly;
+
+    protected readonly Assembly InfrastructureLayer = typeof(CatalogueModule).Assembly;
+
+    private void AssertEmptyArchTestResult(IEnumerable<Type> types)
+    {
+        Assert.That(types, Is.Null.Or.Empty);
+    }
+    
+    protected void AssertEmptyArchTestResult(TestResult result)
+    {
+        AssertEmptyArchTestResult(result.FailingTypes);
+    }
+    
+    private void AssertNotEmptyArchTestResult(IEnumerable<Type> types)
+    {
+        Assert.That(types, Is.Not.Empty);
+    }
+    
+    protected void AssertNotEmptyArchTestResult(TestResult result)
+    {
+        AssertNotEmptyArchTestResult(result.FailingTypes);
+    }
+}
